ce812d9fb5a070b8d99705ea284cb38c8ba2c4c0
sukiElaina <AZyuanjun@163.com>
2024-09-24 12:23:42 +0800

    在其他环境调用TTS失败
    Signed-off-by: sukiElaina <AZyuanjun@163.com>

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from PyQt5.QtWidgets import QApplication
 import sys
 import subprocess
 if __name__ == "__main__":
-    bat_file_path = r"E:\Computer\miHoYo-Inference\!启动!.bat"
-    subprocess.run([bat_file_path], shell=True)
+    # bat_file_path = r"E:\Computer\miHoYo-Inference\!启动!.bat"
+    # subprocess.run([bat_file_path], shell=True)
     app = QApplication(sys.argv)
     window = Pix()
     window.show()
